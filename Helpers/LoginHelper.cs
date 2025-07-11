c5c5f4c2fb4cefc26101069b6480aea880b26fe9
Zstefanov <gsstefanov91@gmail.com>
2025-07-11 23:12:30 +0300

    login helper modified

--- Helpers/LoginHelper.cs
+++ Helpers/LoginHelper.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AE_extensive_project.PerformanceTests.Models;
+
 
 namespace AE_extensive_project.PerformanceTests.Helpers
 {
-    internal class LoginHelper
+    public class LoginHelper
     {
+        private readonly HttpClient _client;
+        private readonly string _loginUrl = "https://automationexercise.com/api/verifyLogin";
+
+        public LoginHelper()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<bool> LoginAsync(UserCredentials user)
+        {
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("email", user.Email),
+                new KeyValuePair<string, string>("password", user.Password)
+            });
+
+            var response = await _client.PostAsync(_loginUrl, content);
+            return response.IsSuccessStatusCode;
+        }
     }
 }
