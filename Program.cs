06061c0f41b429fe0b0273174ce3fab3fba0d1fa
Zstefanov <gsstefanov91@gmail.com>
2025-07-13 19:57:21 +0300

    program.cs swap

--- Program.cs
+++ Program.cs
@@ -27,9 +27,8 @@ namespace AE_extensive_project.PerformanceTests
                 //OrderPlacementLoadTest.CreateScenario(users),
                 //CartOperationsLoadTest.CreateScenario(users),
                 //GetProductEndpointLoadTest.CreateScenario(),\
-                //LoginNegativeTest.CreateScenario(),
+                LoginNegativeTest.CreateScenario(),
                 //UserRegistrationNegativeTest.CreateScenario(users),
-                OrderPlacementNegativeTest.CreateScenario(users) 
             };
 
             NBomberRunner
